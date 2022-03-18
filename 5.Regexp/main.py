6913cead2fb8412be79c1302ca34aee855e3b8e6
Bodrian <u3363777@yandex.ru>
2022-03-18 07:13:15 +0300

    lastname, firstname и surname + telefon pattern

--- 5.Regexp/main.py
+++ 5.Regexp/main.py
@@ -11,11 +11,16 @@ import re
 
 # находит ФИО
 pattern = '^([А-Я]+[а-я]*)+[ {1}|,{1}]+([А-Я]+[а-я]*)+[ {1}|,{1}]+([А-Я]+[а-я]*)'
+pattern_telefon = '(7|8)[ |(]{0,2}(\d{3})[ |)|-]{0,2}(\d{0,7})[-?]?(\d{0,4})[-?]?(\d{0,2})[ |(]{0,2}(доб)?[\.| ]{0,2}(\d{0,4})'
 print(contacts_list[1])
 print(','.join(contacts_list[1]))
 list1 = re.match(pattern, ','.join(contacts_list[1]))
 print(list1)
 print(list1.group(3))
+contacts_list[1][0] = list1.group(1)
+contacts_list[1][1] = list1.group(2)
+contacts_list[1][2] = list1.group(3)
+print(contacts_list[1])
 #word_list = re.findall(pattern, contacts_list)
 #print(type(word_list))
 
