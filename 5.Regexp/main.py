ad74976fc0df05ca2e3148ca9f81624f80f0f913
Bodrian <u3363777@yandex.ru>
2022-03-20 19:35:49 +0300

    Записи о людях соединил

--- 5.Regexp/main.py
+++ 5.Regexp/main.py
@@ -9,18 +9,20 @@ pprint(contacts_list)
 # TODO 1: выполните пункты 1-3 ДЗ
 import re
 
-# находит ФИО
 # использую сайт https://regex101.com/ для составления паттерна
-pattern = '^([А-Я]+[а-я]*)+[ {1}|,{1}]+([А-Я]+[а-я]*)+[ {1}|,{1}]+([А-Я]+[а-я]*)'
+pattern = '^([А-Я]+[а-я]*)+[ {1}|,{1}]+([А-Я]+[а-я]*)+[ {1}|,{1}]+([А-Я]+[а-я]*)?'
 pattern_telefon = '(7|8)[ |(]{0,2}(\d{3})[ |)|-]{0,2}(\d{0,7})[-?]?(\d{0,4})[-?]?(\d{0,2})[ |(]{0,2}(доб)?[\.| ]{0,2}(\d{0,4})'
 
 for list in contacts_list:
   # ФИО ищем
   list1 = re.match(pattern, ','.join(list))
   if list1 != None:
-    list[0] = list1.group(1)
-    list[1] = list1.group(2)
-    list[2] = list1.group(3)
+    if list1.group(1) != None:
+      list[0] = list1.group(1)
+    if list1.group(2) != None:
+      list[1] = list1.group(2)
+    if list1.group(3) != None:
+      list[2] = list1.group(3)
 
   #ищем телефон
   telefon = re.search(pattern_telefon, ','.join(list))
@@ -30,10 +32,17 @@ for list in contacts_list:
     list[5] = list[5].strip()
   print(list)
 
-
-
-#word_list = re.findall(pattern, contacts_list)
-#print(type(word_list))
+# объединяем записи о человеке
+number = []
+for i, list in enumerate(contacts_list):
+  for n in range(i+1, len(contacts_list)):
+    if list[0] == contacts_list[n][0] and list[1] == contacts_list[n][1]:
+      number.append(n)
+      for k, data in enumerate(list):
+        if data == '':
+          list[k] = contacts_list[n][k]
+for i, num in enumerate(number):
+  contacts_list.pop(num - i)
 
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
