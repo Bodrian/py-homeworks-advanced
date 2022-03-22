8df2cfeca053ecfa8d2581e87e910cafea249fb6
Bodrian <u3363777@yandex.ru>
2022-03-22 07:17:31 +0300

    task1 - done

---
+++ 6.Web-scrapping/main.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'no-cors',
+    'Sec-Fetch-Site': 'cross-site',
+}
+
+url = 'https://habr.com/ru/all/'
+
+KEYWORDS = ['костей', 'Кластер хранения', 'web', 'python']
+
+response = requests.get(url, headers=HEADERS)
+soup = BeautifulSoup(response.text, features='html.parser')
+articles = soup.find_all(class_='tm-article-snippet')
+for article in articles:
+    for key in KEYWORDS:
+        article_text = article.find_all(string=re.compile(key))
+        if len(article_text) != 0:
+            time = article.find(class_='tm-article-snippet__datetime-published').find('time')['title']
+            title = article.find(class_='tm-article-snippet__title-link').text
+            link =  article.find(class_='tm-article-snippet__title-link')['href']
+            print(f'Статья: {time} - {title} - {url + link}')
+
+
