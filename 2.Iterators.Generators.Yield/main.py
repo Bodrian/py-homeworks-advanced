20639cd50eec65e848a42f944ac8804634e0cff0
Bodrian <u3363777@yandex.ru>
2022-03-24 06:21:07 +0300

    task1 - done

--- 2.Iterators.Generators.Yield/main.py
+++ 2.Iterators.Generators.Yield/main.py
@@ -7,23 +7,24 @@ nested_list = [
 class Mylist:
 	def __init__(self, nested_list):
 		self.nested_list = nested_list
+		self.list = []
+		self.i = 0
 
 	def __iter__(self):
-		i = 0
-		list = []
-		while i != len(nested_list):
-			list += nested_list[i]
-			i += 1
-		return list
+		return self
 
-	def __next__(self, list):
-		if len(list) == 0:
+	def __next__(self):
+		while self.i != len(nested_list):
+			self.list += nested_list[self.i]
+			self.i += 1
+		if len(self.list) == 0:
 			raise StopIteration
-		return list.pop()
+		return self.list.pop(0)
 
 
 
 for item in Mylist(nested_list):
 	print(item)
 
-#flat_list = [item for item in Myrange(nested_list)]
+flat_list = [item for item in Mylist(nested_list)]
+print(flat_list)
