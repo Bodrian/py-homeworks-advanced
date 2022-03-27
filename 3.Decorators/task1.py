dc543a6991bdcb551c015d20a963a80283adf80b
Bodrian <u3363777@yandex.ru>
2022-03-27 13:25:06 +0300

    task1 + task2 - кроме записи в файл

--- 3.Decorators/task1.py
+++ 3.Decorators/task1.py
@@ -2,19 +2,25 @@
 # Написать декоратор - логгер. Он записывает в файл дату и время вызова функции,
 # имя функции, аргументы, с которыми вызвалась и возвращаемое значение.
 
-import time
+from time import asctime
 
-def decor(old_function):
-    def new_function(a, b, c):
-        print(f'Вызвана функция {old_function}')
-        print(f'с аргументами {a} {b} {c}')
-        result = old_function()
-        print(f'получен результат {result}')
-        return result
-    return new_function
+def decor(log_path = '/log'):
+    def _decor(old_function):
+        def new_function(*args, **kwargs):
+            print(f'Вызвана функция {old_function}')
+            print(f'с аргументами {args} {kwargs}')
+            result = old_function(*args, **kwargs)
+            print(f'получен результат {result}')
+            print(f'Время вызова {asctime()}')
+            print(f'Путь к логу {log_path}')
+            return result
+        return new_function
+    return _decor
 
-@decor
-def summ(a=1, b=2, c=3):
+decor_log = decor('/log1')
+
+@decor_log
+def summ(a = 1, b = 2, c = 3):
     return a + b + c
 
 
