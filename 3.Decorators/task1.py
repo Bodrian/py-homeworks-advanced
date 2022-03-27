5dab5281f17350751796375595ce0b6c2c4487d8
Bodrian <u3363777@yandex.ru>
2022-03-27 14:41:08 +0300

    all - done

--- 3.Decorators/task1.py
+++ 3.Decorators/task1.py
@@ -4,25 +4,22 @@
 
 from time import asctime
 
-def decor(log_path = '/log'):
+def decor(log_path):
     def _decor(old_function):
         def new_function(*args, **kwargs):
-            print(f'Вызвана функция {old_function}')
-            print(f'с аргументами {args} {kwargs}')
             result = old_function(*args, **kwargs)
-            print(f'получен результат {result}')
-            print(f'Время вызова {asctime()}')
-            print(f'Путь к логу {log_path}')
+            with open(f'{log_path}log.txt', 'a', encoding='UTF-8') as f:
+                f.write(f'Вызвана функция: {old_function}\nC аргументами: {args} {kwargs}\n'
+                        f'Получен результат: {result}\nВремя вызова: {asctime()}\nПуть к логу: {log_path}')
             return result
         return new_function
     return _decor
 
-decor_log = decor('/log1')
+decor_log = decor('log1/')
 
-@decor_log
+@decor_log # task1 + task2
 def summ(a = 1, b = 2, c = 3):
     return a + b + c
-
 
 if __name__ == "__main__":
 
